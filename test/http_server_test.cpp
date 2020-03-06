a47da7df3ba20b0d4d89852cac80b206322881d5
mojiajun <mojiajunmail@gmail.com>
2020-03-06 17:10:11 +0800

    在mac这边用Chrome访问在阿里云上的服务器

--- test/http_server_test.cpp
+++ test/http_server_test.cpp
@@ -21,7 +21,7 @@ void onRequest(const HttpRequest &req, HttpResponse *resp) {
         resp->setStatusCode(HttpResponse::k200Ok);
         resp->setStatusMessage("OK");
         resp->setContentType("text/html");
-        resp->addHeader("Server", "Muduo");
+        resp->addHeader("Server", "httpserver");
         std::string now = Timestamp::now().toFormattedString();
         resp->setBody("<html><head><title>This is title</title></head>"
                       "<body><h1>Hello</h1>Now is " + now +
@@ -49,9 +49,12 @@ int main(int argc, char **argv) {
     LOG(INFO) << "http server launch.";
 
     EventLoop loop;
+    // 在云服务器上使用curl访问, 或者直接用一个浏览器打开http://阿里云的外网ip:8000/
+    // 要在网页控制端打开防火墙
+    // 设置为阿里云的内网ip, InetAddress("阿里云的内网ip", 8000)
+    // 参考https://www.cnblogs.com/wuzaipei/p/10729326.html
     HttpServer server(&loop, InetAddress("127.0.0.1", 8000), "http_test_server");
     server.setHttpCallback(onRequest);
     server.startListen();
     loop.loop();
 }
-
