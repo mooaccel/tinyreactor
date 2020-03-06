450e2b461d9b9360a6b84925959dc400effbc69b
mojiajun <mojiajunmail@gmail.com>
2020-03-06 16:09:32 +0800

    http服务初步测试通过,用curl

--- src/http/http_context.cpp
+++ src/http/http_context.cpp
@@ -5,6 +5,39 @@
 
 using namespace tinyreactor;
 
+/// TODO
+bool HttpContext::processRequestLine(const char *begin, const char *end) {
+    bool succeed = false;
+    const char *start = begin;
+    const char *space = std::find(start, end, ' ');
+    if (space != end && request_.setMethod(start, space)) {
+        start = space + 1;
+        space = std::find(start, end, ' ');
+        if (space != end) {
+            const char *question = std::find(start, space, '?');
+            if (question != space) {
+                request_.setPath(start, question);
+                request_.setQuery(question, space);
+            } else {
+                request_.setPath(start, space);
+            }
+            start = space + 1;
+            succeed = end - start == 8 && std::equal(start, end - 1, "HTTP/1.");
+            if (succeed) {
+                if (*(end - 1) == '1') {
+                    request_.setVersion(HttpRequest::kHttp11);
+                } else if (*(end - 1) == '0') {
+                    request_.setVersion(HttpRequest::kHttp10);
+                } else {
+                    succeed = false;
+                }
+            }
+        }
+    }
+    return succeed;
+}
+
+/// TODO
 // return false if any error
 bool HttpContext::parseRequestFromBuffer(Buffer *buf) {
     bool ok = true;
